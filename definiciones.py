08ce4191412698496dc7c12a3e34002e4c6cd837
Fernando Rodríguez <f.rodriguez96sur@gmail.com>
2025-06-11 10:37:38 -0300

    Varios cambios en el código, falta terminar de implementar algunas cosas

--- definiciones.py
+++ definiciones.py
@@ -1,20 +1,2 @@
 MAX_LARGO_MENSAJE = 0xff
 MAX_LARGO_BUFFER = 1 << 10
-
-def is_valid_ipv4(text : str):
-	ip_values = text.split('.')
-	ip_values_size = len(ip_values)
-	result = False
-	
-	if ip_values_size == 4:
-		result = True
-		
-		try:
-			for i in range(ip_values_size):
-				val = int(ip_values[i])
-				if(val < 0 or val > 255):
-					result = False
-		except ValueError:
-			result = False
-	
-	return result
--- p2pnode.py
+++ p2pnode.py
@@ -1,7 +1,8 @@
 import sys
 import socket
 import threading
-import datetime
+import time
+import json
 
 import definiciones
 
@@ -10,39 +11,58 @@ class P2PNode:
 		self.host = '127.0.0.1'
 		self.port = port
 		self.username = username
-		# diccionario host-username
-		self.known_peers = {}
 		self.running = False
+		self.hosts_connected = []
 	
 	def start():
 		self.server = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
 		self.server.bind(self.host, self.port)
 		self.listen(5)
-		
 		self.running = True
 		
 		accept_conn_thread = threading.Thread(self.accept_connection)
+		accept_conn_thread.start()
 		
+		user_interface_thread = threading.Thread(self.user_interface)
+		user_interface_thread.start()
 
 	def accept_connection():
 		while self.running:
 			try:
 				conn, addr = self.server.accept()
+				self.hosts_connected.append(addr[0])
+				self.running = True
 				threading.Thread(self.handle_connection(conn, addr)).start()
 			except:
 				if self.running:
 					print('Error al aceptar la conexión')
 	
-	def handle_conenection():
-		pass
+	def handle_conenection(self, conn, addr):
+		while self.running:
+			try:
+				data_json = conn.recv(definiciones.MAX_LARGO_BUFFER)
+				
+				data = json.loads(data_json)
+				
+				msg = data['content']
+				user = data['user']
+				now = time.strftime('[%Y.%m.%d %H:%M]')
+				print(now + addr[0] + user + " dice: " + msg)				
+			except json.JSONDecodeError:
+				print(f"Datos inválidos recibidos de {addr}")
+			except Exception as e:
+				print(f'Error: {e}')
+				
+				self.hosts_connected.remove(addr[0])
+				conn.close()
+				break
+		
+		if len(self.hosts_connected) == 0:
+			self.running = False
+		else:
+			self.running = True
 	
-	def receive_messages(self, conn, addr):
-		mensaje = ''
-		try:
-			data = conn.recv(MAX_LARGO_BUFFER).decode('utf-8').strip()
+	def user_interface():
+		while True:
+			line = input('>>> ')
 			
-			# obtiene el string desde la posición cero hasta la posición MAX_LARGO_MENSAJE - 1 inclusive
-			data = data[0 : 0 + MAX_LARGO_MENSAJE]
-			
-			fecha = datetime.now().strftime('[%Y.%M.%D %H:%M]')
-			mensaje = fecha + ' ' + addr[0] + 
